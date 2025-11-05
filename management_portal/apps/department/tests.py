358ef59f5103e453facaba3009327b658b422185
molumolu <kan.pavel@ilcoud.com>
2025-11-05 21:44:21 +0500

    department tests

--- management_portal/apps/department/tests.py
+++ management_portal/apps/department/tests.py
@@ -1,3 +1,59 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
+from rest_framework import status
+from apps.users.models import User
+from apps.department.models import Department
 
+class DepartmentListTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345', is_staff=True)
+        self.client.force_authenticate(user=self.user)
+        departments_to_create = [
+            Department(title='HR', slug='hr', chief=self.user),
+            Department(title='Engineering', slug='eng', chief=self.user),
+            Department(title='Sales', slug='sales', chief=self.user),
+            Department(title='Marketing', slug='mark', chief=self.user),
+        ]
+
+        Department.objects.bulk_create(departments_to_create)
+        self.departments = Department.objects.all()
+
+    def test_list_departments(self):
+        
+        url = reverse('departments_view') 
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreater(Department.objects.count(), 0)
+
+    def test_create_department(self):
+        url = reverse('departments_view') 
+        data = {
+            'title': 'Test Department',
+            'slug': 'test-department-1',
+            'description': 'department description',
+            'chief': self.user.id
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['title'], 'Test Department')
+    
+    def test_retrieve_department(self): 
+        url = reverse('department_profile_view', kwargs={'slug': self.departments[0].slug})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], self.departments[0].title)
+
+    def test_update_department(self):
+        url = reverse('department_profile_view', kwargs={'slug': self.departments[0].slug})
+        data = {
+            'title': 'Updated title',
+        }
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.data['title'], 'Updated title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_department(self):
+        url = reverse('department_profile_view', kwargs={'slug': self.departments[0].slug})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+   
--- management_portal/apps/department/views.py
+++ management_portal/apps/department/views.py
@@ -1,16 +1,16 @@
 from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from .models import Department, DepartmentMember
 from .serializers import DepartmentSerializer, DepartmentMemberSerializer
 from apps.users.models import User
 from apps.users.permissions import IsChief
 from apps.sections.models import DepartmentSection
 from apps.sections.serializers import DepartmentSectionSerializer
-from django.db.models import Q, F
 from django.shortcuts import get_object_or_404
+from rest_framework.pagination import LimitOffsetPagination
+from django.db.models import Q
 
 class DepartmentsListView(ListCreateAPIView):
     permission_classes=[AllowAny]
@@ -19,14 +19,20 @@ class DepartmentsListView(ListCreateAPIView):
 
 class DepartmentProfileView(RetrieveUpdateDestroyAPIView):
     permission_classes=[IsAdminUser]
+    model = Department
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+    lookup_field='slug'
+    lookup_url_kwarg='slug'
 
 class DepartmentMemberView(ListCreateAPIView):
     permission_classes = [IsAdminUser | IsChief]
+    model = DepartmentMember
     serializer_class= DepartmentMemberSerializer
     def get_queryset(self):
-        return DepartmentMember.objects.filter(department_id=self.kwargs["department_id"])
+        # Override get_queryset for the LIST part of ListCreateAPIView
+        department_id = self.kwargs.get("department_id")
+        return DepartmentMember.objects.filter(department_id=department_id)
 
     def create(self, request, *args, **kwargs):
         data = request.data
@@ -52,39 +58,45 @@ class DepartmentMemberUpdateView(RetrieveUpdateDestroyAPIView):
         self.check_object_permissions(self.request, obj)
         return obj
     
-class DepartmentSectionsView(APIView):
+class DepartmentSectionsView(ListAPIView):
+    permission_classes = AllowAny    
+    model = DepartmentSection
+    serializer_class = DepartmentSectionSerializer
+    pagination_class = LimitOffsetPagination
 
-    permission_classes = [AllowAny] 
+    def get_queryset(self):
+        user = self.request.user
+        role = DepartmentMember.objects.get(user=user).role
+        department_slug = self.kwargs.get('slug')
+       
+        mandatory_filters = {
+            'department__slug': department_slug,
+            'status': 'published',
+        }
+         
+        if user.is_staff or role=="Chief":
+            return DepartmentSection.objects.filter(*mandatory_filters)
+        
+        visibility_criteria = Q(visibility='public')
+        
+        if user.is_authenticated:
+            # Add sections available for all memebers
+            member_sections_q = Q(
+                visibility='members',
+                department__members__user=user,
+            )
+            # Add available role based sections         
+            staff_sections_q = Q(visibility='role_based', visible_for_roles__contains=role)
+            visibility_criteria |= staff_sections_q or member_sections_q
 
-    def get(self, request, slug):
-        qs = DepartmentSection.objects.filter(department__slug=slug)
+        queryset = DepartmentSection.objects.filter(
+            **mandatory_filters
+        ).filter(
+            visibility_criteria
+        ).order_by('order')
 
-        if not request.user.is_authenticated:
-            qs = qs.filter(visibility="public", status="published")
-
-        elif request.user.is_staff:
-            pass
-
-        elif getattr(request.user, "is_clergy", False) or getattr(request.user, "is_rectorate", False):
-            qs = qs.filter(status="published")
-
-        else:
-            qs = qs.filter(
-                        
-                Q(visibility="public", status="published")
-                | Q(
-                    department__members__user=request.user,
-                    department__members__is_active=True,
-                    department__members__role="chief"
-                )
-                | Q(
-                    department__members__user=request.user,
-                    department__members__is_active=True,
-                    department__members__role__in=F("visible_for_roles")
-                )).distinct()
-            
-        serializer = DepartmentSectionSerializer(qs, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return queryset
+    
         
      
 
